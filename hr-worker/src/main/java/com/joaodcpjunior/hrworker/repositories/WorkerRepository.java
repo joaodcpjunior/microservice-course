67c371c2914615a5887a9176521c6d442d0b94c1
João de Deus <joaodcpjunior@gmail.com>
2022-07-07 11:31:40 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/joaodcpjunior/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,7 @@
+package com.joaodcpjunior.hrworker.repositories;
+
+import com.joaodcpjunior.hrworker.entities.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+}
