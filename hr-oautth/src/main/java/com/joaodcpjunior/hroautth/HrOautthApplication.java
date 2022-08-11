47b1bc1387d976d2d8f5115dbb8a074a2e4b4681
João de Deus <joaodcpjunior@gmail.com>
2022-08-11 11:25:40 -0300

    Project hr-oauth created

---
+++ hr-oautth/src/main/java/com/joaodcpjunior/hroautth/HrOautthApplication.java
@@ -0,0 +1,13 @@
+package com.joaodcpjunior.hroautth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrOautthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrOautthApplication.class, args);
+    }
+
+}
---
+++ hr-oautth/src/test/java/com/joaodcpjunior/hroautth/HrOautthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaodcpjunior.hroautth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOautthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
